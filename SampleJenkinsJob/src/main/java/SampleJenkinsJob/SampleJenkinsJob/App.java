68ebc88311127609c58c586621661b86a30ebe74
sumanthLattice <sravipati@lattice-engines.com>
2019-06-10 10:27:47 -0700

    Sample Project Chnages Commit

---
+++ SampleJenkinsJob/src/main/java/SampleJenkinsJob/SampleJenkinsJob/App.java
@@ -0,0 +1,13 @@
+package SampleJenkinsJob.SampleJenkinsJob;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
